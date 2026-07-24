331063c185b8ad812ffcde9735c71399f36421f7
Coen Büller <coen.buller@gmail.com>
2026-07-24 11:27:14 +0200

    Dataset classes. Changed the Jarvis dataset to a class which loads in the data. This can than be used to create a test/validate split and the data can be put in their repsective dataset classes

--- Training/Dataset.py
+++ Training/Dataset.py
@@ -1,4 +1,3 @@
-import torchaudio 
 import os
 import pandas as pd
 import torch
@@ -6,29 +5,89 @@ import soundfile as sf
 import numpy as np
 
 from torch.utils.data import Dataset
+from Augment.augment_config import AugmentConfig
+from torchaudio.transforms import MelSpectrogram
 
-class WakeWordDataset(Dataset):
-    def __init__(self, data_folder, target_samplerate, p_silence=0.2, rng=np.random.default_rng(seed=49), transformer=None, target_transformer=None):
-        self.target_samplerate = target_samplerate
-        self.dir = data_folder
+class TrainDataset(Dataset):
+    def __init__(self, items, target_samplerate, p_silence=0.2, rng=np.random.default_rng(seed=49), transformer=None, target_transformer=None):
+
+        self.items = items
+        self.sr = target_samplerate
+        self.p_silence = p_silence
+        self.rng = rng
         self.transformer = transformer
         self.target_transformer = target_transformer
-        self.labels = self.__makelabels()
-        self.rng = rng
-        self.p_silence = p_silence
 
     def load_audio(self, path):
-        data, sr = sf.read(path, dtype='float32')           # shape: (n_samples,) or (n_samples, channels)
-        if data.ndim != 1:
-            data = np.mean(data, axis=1, keepdims=False)    # Convert to mono channels audio (n_samples,)
-
-        resampled_data = torchaudio.transforms.Resample(orig_freq=sr, new_freq=self.target_samplerate)(data) # Resample to target samplerate to keep it consistent 
-        tensor = torch.from_numpy(resampled_data)    
+        data, _ = sf.read(path, dtype='float32') 
+        tensor = torch.from_numpy(data)
         return tensor
+
+    def __getitem__(self, idx):
+
+        if self.rng.random(size=1) <= self.p_silence:
+            audio = torch.zeros(self.sr, dtype=torch.float32)
+            label = 0
+        else:
+            item = self.items.iloc[idx]
+            path, label = item["path"], item["label"]
+            audio = self.load_audio(path=path) # Audio has shape (n_samples) with the target samplerate
+
+        # Apply transformations if given. They must be callable transformations
+        if self.transformer:
+            audio = self.transformer(audio)
+        if self.target_transformer:
+            label = self.target_transformer(label)
+
+        return audio, label
+    
+    def __len__(self):
+        return len(self.items)
+
+    def return_labels(self):
+        return self.items["label"].to_numpy()
+
+class ValidateDataset(Dataset):
+    def __init__(self, items):
+        self.items = items
+
+        cfg = AugmentConfig()
+        self.spec = MelSpectrogram(sample_rate=cfg.sr, 
+                                   n_fft=cfg.n_fft, 
+                                   hop_length=cfg.hop_length)
+
+    def load_audio(self, path):
+        data, _ = sf.read(path, dtype='float32') 
+        tensor = torch.from_numpy(data)
+        return tensor
+
+    def __getitem__(self, idx):
+
+        item = self.items.iloc[idx]
+        path, label = item["path"], item["label"]
+        audio = self.load_audio(path=path) # Audio has shape (n_samples) with the target samplerate
+        mel = self.spec(audio)
+        return mel, label
+
+    def __len__(self):
+        return len(self.items)
+
+
+
+class LoadDataset(Dataset):
+    """Initial dataset structure used to create labels and will be used to create train test split"""
+    def __init__(self, data_folder, rng=np.random.default_rng(seed=49)):
+
+        self.dir = data_folder
+        self.rng = rng
+        self.labels = []
+        self.paths = []
+        self.items = self.__makelabels()
+
+
 
     def __makelabels(self):
         folders = [f for f in os.listdir(self.dir) if os.path.isdir(os.path.join(self.dir, f))]
-        paths, labels = [], []
         for folder in folders:
             folder_path = os.path.join(self.dir, folder)
 
@@ -42,32 +101,26 @@ class WakeWordDataset(Dataset):
 
             for file in os.listdir(folder_path):
                 path = os.path.join(folder_path, file)
-                paths.append(path)
-                labels.append(label) 
+                self.paths.append(path)
+                self.labels.append(label) 
         
-        return pd.DataFrame(data={"path": paths, "label": labels})
+        return pd.DataFrame(data={"path": self.paths, "label": self.labels})
     
     def __getitem__(self, idx):
+
+        item = self.items.iloc[idx]
+        path, label = item["path"], item["label"]
+
+        return path, label
+
+    def __getitems__(self, indices):
+        items = self.items.iloc[indices]
+        return items
         
-        if self.rng.random(size=1) <= self.p_silence:
-            audio = torch.zeros(self.target_samplerate, dtype=torch.float32)
-            label = 0
-        else:
-            item = self.labels.iloc[idx]
-            path, label = item["path"], item["label"]
-            audio = self.load_audio(path=path) # Audio has shape (n_samples) with the target samplerate
-
-        # Apply transformations if given. They must be callable transformations
-        if self.transformer:
-            audio = self.transformer(audio)
-        if self.target_transformer:
-            label = self.target_transformer(label)
-
-        return audio, label
-        
-
     def __len__(self):
-        return len(self.labels)
-    
+        return len(self.items)
+
     def return_labels(self):
-        return self.labels["label"]
+        return self.items["label"]
+
+    
