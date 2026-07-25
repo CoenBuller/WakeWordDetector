a5a685b1a5e18b4b2267f9cf42a859ef34910562
Coen Büller <coen.buller@gmail.com>
2026-07-26 00:29:05 +0200

    Removed a convolutional layer which felt like a bit too much as a simpeler model will likely perform aswell since it is a relatively simple problem at hand

--- model.py
+++ model.py
@@ -19,9 +19,6 @@ class Jarvis(nn.Module):
                             nn.Conv2d(16, 32, kernel_size=3, padding=1), # shape: (16, h, w) -> (32, h, w)
                             nn.ReLU(),
                             nn.BatchNorm2d(num_features=32),
-                            nn.Conv2d(32, 32, kernel_size=3, padding=1), # shape: (32, h, w) -> (32, h, w)
-                            nn.ReLU(),
-                            nn.BatchNorm2d(num_features=32),
                             nn.AdaptiveAvgPool2d(output_size=(1, 8)) # shape: (32, h, w) -> (8, h, w)
                         )
         
